d6f8e949db244852d9dd65280862e2a70195d7b0
sazumy <49363119+sazumy@users.noreply.github.com>
2021-12-08 22:47:33 +0900

    EventsControllerと関連づけの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
   root "welcome#index"
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
+
+  resources :event
 end
