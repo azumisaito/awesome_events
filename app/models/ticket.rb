9ca927b2c2f4ff11ca785d2a43a743823bf73ebb
sazumy <49363119+sazumy@users.noreply.github.com>
2021-12-20 18:30:55 +0900

    イベント参加用モーダルの作成

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,6 @@
 class Ticket < ApplicationRecord
-  belongs_to :user, optioal: true
+  belongs_to :user, optional: true
   belongs_to :event
 
-  validates: comment, length: { maximum: 30 }, allow_blank: true
+  validates :comment, length: { maximum: 30 }, allow_blank: true
 end
